3884dad8ccb7ca9808c99189c3fd1435ff3e0f4a
Olli Perikanta <33216236+OlliPerikanta@users.noreply.github.com>
2018-09-30 11:55:30 +0300

    slidwshowin lisääminen etusivulle
    Testi

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+
+
+    $("#slideshow > div:gt(0)").hide();
+
+    setInterval(function () {
+        $('#slideshow > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+    }, 3000);
