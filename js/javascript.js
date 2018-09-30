fb8d240330b5f3c06d3aa2c3760b19b99437a062
Olli Perikanta <33216236+OlliPerikanta@users.noreply.github.com>
2018-09-30 12:17:32 +0300

    slideshowin korjausta

--- js/javascript.js
+++ js/javascript.js
@@ -6,14 +6,16 @@
         document.getElementById("myNav").style.width = "0%";
     }
 
+    $(document).ready(function () {
+        $("#slideshow > div:gt(0)").hide();
 
-    $("#slideshow > div:gt(0)").hide();
+        setInterval(function () {
+            $('#slideshow > div:first')
+                .fadeOut(1000)
+                .next()
+                .fadeIn(1000)
+                .end()
+                .appendTo('#slideshow');
+        }, 3000);
 
-    setInterval(function () {
-        $('#slideshow > div:first')
-            .fadeOut(1000)
-            .next()
-            .fadeIn(1000)
-            .end()
-            .appendTo('#slideshow');
-    }, 3000);
+    });
